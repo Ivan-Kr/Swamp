550d477c5383073fe4877ea18d1aaf6edb04397f
Ivan-Kr <butunska5@gmail.com>
2021-11-26 21:49:42 +0200

    made simple game and try to insert my previewly lib to this repository

---
+++ Swamp/Kiwii/ScreenText.cpp
@@ -0,0 +1,64 @@
+#include "ScreenText.h"
+
+void kiwii::ScreenText::Setup() {}
+
+void kiwii::ScreenText::SetupS(WORD Width, WORD Height)
+{
+	_width = Width;
+	_height = Height;
+
+	SetWindow(_width, _height);
+
+	_hconsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	SetConsoleActiveScreenBuffer(_hconsole);
+
+	_aspect = _width / _height;
+
+	_screen = new wchar_t[SQUARE() + 1];
+	_screen[SQUARE()] = L'\0';
+}
+
+wchar_t kiwii::ScreenText::ScreenL(int index) {
+	if (!(index < 0 || index >= SQUARE()))
+		return _screen[index];
+	else
+		return L'\0';
+}
+
+void kiwii::ScreenText::ScreenL(int index, wchar_t val) {
+	if (!(index < 0 || index >= SQUARE()))
+		_screen[index] = val;
+}
+
+wchar_t kiwii::ScreenText::ScreenP(int x, int y) {
+	if (!(x+y*_width < 0 || x + y * _width >= SQUARE()))
+		return _screen[x + y * _width];
+	else
+		return L'\0';
+}
+
+void kiwii::ScreenText::ScreenP(int x, int y, wchar_t val) {
+	if (!(x + y * _width < 0 || x + y * _width >= SQUARE()))
+		_screen[x + y * _width] = val;
+}
+
+void kiwii::ScreenText::Fill(wchar_t val)
+{
+	for (int i = 0;i < kiwii::ScreenText::SQUARE();i++) {
+		_screen[i] = val;
+	}
+}
+
+void SetWindow(int Width, int Height) {
+	_COORD coord;
+	coord.X = Width;
+	coord.Y = Height;
+	_SMALL_RECT Rect;
+	Rect.Top = 0;
+	Rect.Left = 0;
+	Rect.Bottom = Height - 1;
+	Rect.Right = Width - 1;
+	HANDLE Handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleScreenBufferSize(Handle, coord);
+	SetConsoleWindowInfo(Handle, TRUE, &Rect);
+}
--- Swamp/Swamp.cpp
+++ Swamp/Swamp.cpp
@@ -1,6 +1,115 @@
 ﻿#include <iostream>
+#include "./Kiwii/ScreenText.h"
+#include <sstream>
+#include <conio.h>
+
+class Obgect {
+    _COORD _pos;
+    wchar_t _symbol = L'!';
+
+public:
+    short X() {
+        return _pos.X;
+    }
+    short Y() {
+        return _pos.Y;
+    }
+    void Pos(short x, short y) {
+        _pos.X = x;
+        _pos.Y = y;
+    }
+
+    wchar_t Symbol() {
+        return _symbol;
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //kiwii::ScreenText ux;
+
+    //ux.SetupS(24, 8);
+    Obgect a;
+    a.Pos(3,3);
+    std::wstring map[8] =
+    {
+        L"#---------------------#",
+        L"|                     |",
+        L"|                     |",
+        L"|                     |",
+        L"|                     |",
+        L"|                     |",
+        L"|                     |",
+        L"#---------------------#",
+    };
+    while (true) {
+        system("cls");
+
+        if (GetAsyncKeyState(VK_UP)) {
+            if (map[a.Y() - 1][a.X()] == L' ')
+                a.Pos(a.X(), (a.Y() - 1));
+        }
+        if (GetAsyncKeyState(VK_LEFT)) {
+            if (map[a.Y()][(a.X() - 1)] == L' ')
+                a.Pos((a.X() - 1), a.Y());
+        }
+        if (GetAsyncKeyState(VK_DOWN)) {
+            if (map[a.Y() + 1][a.X()] == L' ')
+                a.Pos(a.X(), (a.Y() + 1));
+        }
+        if (GetAsyncKeyState(VK_RIGHT)) {
+            if (map[a.Y()][(a.X() + 1)] == L' ')
+                a.Pos((a.X() + 1), a.Y());
+        }
+        if (GetAsyncKeyState(VK_SHIFT)) {
+            system("pause");
+        }
+
+        /*if (_kbhit()) {
+            switch (_getwch())
+            {
+            case L'w':
+                if(map[a.Y() - 1][a.X()]==L' ')
+                    a.Pos(a.X(),(a.Y() - 1));
+                break;
+            case L'a':
+                if (map[a.Y()][(a.X() - 1)] == L' ')
+                    a.Pos((a.X() - 1), a.Y());
+                break;
+            case L's':
+                if (map[a.Y() + 1][a.X()] == L' ')
+                    a.Pos(a.X(), (a.Y() + 1));
+                break;
+            case L'd':
+                if (map[a.Y()][(a.X() + 1)] == L' ')
+                    a.Pos((a.X() + 1), a.Y());
+                break;
+            default:
+                break;
+            }
+        }
+
+        */
+
+        map[0] = L"#---------------------#";
+        map[1] = L"|                     |";
+        map[2] = L"|                     |";
+        map[3] = L"|                     |";
+        map[4] = L"|                     |";
+        map[5] = L"|                     |";
+        map[6] = L"|                     |";
+        map[7] = L"#---------------------#";
+
+        map[a.Y()][a.X()] = a.Symbol();
+
+
+        for (int i = 0;i < 8;i++) {
+
+
+            std::wcout << map[i]<<'\n';
+        }
+        //ux.Out();
+    }
+
+    return 0;
 }
